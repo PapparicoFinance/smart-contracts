d37c8b4840dbf08403c0d9fd9313a50175dd9adc
Jamesson Neves <jamesson.neves@gmail.com>
2024-03-03 02:11:13 +0100

    Deleted unused files.

---
+++ scripts/production/2.2_ScriptStakingSupply.js
@@ -0,0 +1,28 @@
+//truffle exec scripts/production/2.2_ScriptStakingSupply.js --network cronosmainnet
+
+require("dotenv").config();
+const { DEPLOYER } = process.env;
+
+const PapparicoStaking = artifacts.require("PapparicoStaking");
+const PapparicoToken = artifacts.require("PapparicoToken");
+const { toBN } = require('web3-utils');
+
+module.exports = async function(callback) {
+
+  const toBigNumber = toBN;
+  console.log("DEPLOYER: " + DEPLOYER);
+  
+  const papparicoStakingInstance = await PapparicoStaking.deployed();
+  const papparicoTokenInstance = await PapparicoToken.deployed();
+
+  let rewardsEmissionPerBlock = 3000;
+
+  const DECIMAL_DIGITS = toBigNumber("1000000000000000000");
+  let supplyAmount = (rewardsEmissionPerBlock * 60 * (14400 * 90)) / 600000;
+  let supplyAmountBig = toBigNumber(supplyAmount).mul(DECIMAL_DIGITS);
+  console.log("SUPPLIED = " + supplyAmountBig);
+
+  await papparicoTokenInstance.mint(papparicoStakingInstance.address, supplyAmountBig, 1, {from: DEPLOYER});
+
+	callback();
+}
--- scripts/testing-cronos.testnet/3_ScriptInitializeStaking.js
+++ scripts/testing-cronos.testnet/3_ScriptInitializeStaking.js
@@ -1,36 +1,24 @@
-//truffle exec scripts/testing-cronos.testnet/3_ScriptInitializeStaking.js --network cronostestnet
+//truffle exec scripts/production/2_ScriptInitializeStaking.js --network cronosmainnet
 
 require("dotenv").config();
 const { DEPLOYER } = process.env;
 
-const PapparicoToken = artifacts.require("PapparicoToken");
 const PapparicoStaking = artifacts.require("PapparicoStaking");
-const { toBN } = require('web3-utils');
 
 module.exports = async function(callback) {
 
-  const toBigNumber = toBN;
   console.log("DEPLOYER: " + DEPLOYER);
   
-  const papparicoTokenInstance = await PapparicoToken.deployed();
   const papparicoStakingInstance = await PapparicoStaking.deployed();
 
   let currentBlockNumber = await web3.eth.getBlockNumber();
 
   //Initialize PapparicoStaking - Params: 1 = startBlock, 2 = rewardsEmissionPerBlock
-  let rewardsEmissionPerBlock = 30;
+  let rewardsEmissionPerBlock = 3000;
   await papparicoStakingInstance.initialize(currentBlockNumber + 50, rewardsEmissionPerBlock, {from: DEPLOYER});
 
   //Set withdrawalFee
   await papparicoStakingInstance.setWithdrawalFee(1 * 100, {from: DEPLOYER}); //Multiply the desired percent by 100
-
-  const DECIMAL_DIGITS = toBigNumber("1000000000000000000");
-  let supplyAmount = (rewardsEmissionPerBlock * 60 * (14400 * 90)) / 600000; //3 months
-  let supplyAmountBig = toBigNumber(supplyAmount).mul(DECIMAL_DIGITS);
-  console.log("SUPPLIED = " + supplyAmountBig);
-
-  //Supply initial rewards
-  await papparicoTokenInstance.mint(papparicoStakingInstance.address, supplyAmountBig, 1, {from: DEPLOYER});
 
 	callback();
 }
