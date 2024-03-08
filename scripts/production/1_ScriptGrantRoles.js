01611545aaee230c8cb2f41de470459d54c1f0bc
Jamesson Neves <jamesson.neves@gmail.com>
2024-03-08 18:07:21 +0100

    Refactoring Access Control.

--- scripts/production/1_ScriptGrantRoles.js
+++ scripts/production/1_ScriptGrantRoles.js
@@ -6,8 +6,6 @@ const { DEPLOYER } = process.env;
 const keccak256 = require('keccak256');
 const PapparicoToken = artifacts.require("PapparicoToken");
 
-const PapparicoStaking = artifacts.require("PapparicoStaking");
-const PapparicoVaults = artifacts.require("PapparicoVaults");
 const PapparicoTournaments = artifacts.require("PapparicoTournaments");
 
 const PapparicoTreasury = artifacts.require("PapparicoTreasury");
@@ -18,18 +16,9 @@ module.exports = async function(callback) {
   console.log("DEPLOYER: " + DEPLOYER);
   
   const papparicoTokenInstance = await PapparicoToken.deployed();
-  const papparicoStakingInstance = await PapparicoStaking.deployed();
   const papparicoTournamentsInstance = await PapparicoTournaments.deployed();
   const papparicoTreasuryInstance = await PapparicoTreasury.deployed();
   const papparicoTeamVestingInstance = await PapparicoTeamVesting.deployed();
-  const papparicoVaultsInstance = await PapparicoVaults.deployed();
-
-  //PapparicoStaking must have MINTER role on PapparicoToken
-  let minterRole = keccak256('MINTER');
-  await papparicoTokenInstance.grantRole(minterRole, papparicoStakingInstance.address, {from: DEPLOYER});
-
-  //PapparicoVaults must have MINTER role on PapparicoToken
-  await papparicoTokenInstance.grantRole(minterRole, papparicoVaultsInstance.address, {from: DEPLOYER});
 
   //PapparicoTeamVesting must have MINTER role on PapparicoToken
   await papparicoTokenInstance.grantRole(minterRole, papparicoTeamVestingInstance.address, {from: DEPLOYER});
